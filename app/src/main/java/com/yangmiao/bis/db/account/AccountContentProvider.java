d79cfc2568087bc671c0c35d08af57d555b9527a
ygl <283536411@qq.com>
2016-07-06 15:45:33 +0800

    ygl

--- app/src/main/java/com/yangmiao/bis/db/account/AccountContentProvider.java
+++ app/src/main/java/com/yangmiao/bis/db/account/AccountContentProvider.java
@@ -2,6 +2,7 @@ package com.yangmiao.bis.db.account;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,13 +10,17 @@ import android.net.Uri;
 
 import com.yangmiao.bis.db.IProivderMetaData;
 import com.yangmiao.bis.db.DbHelper;
+import com.yangmiao.bis.model.AccountInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountContentProvider extends ContentProvider {
 
     private static UriMatcher uriMatcher;
     private static final int TYPE_ACCOUNT_LIST = 1;
 
-    private static final String TABLE_NAME = IProivderMetaData.AccountMetaData.TABLE_NAME;
+    private static final String TABLE_NAME = IProivderMetaData.AccountColumns.TABLE_NAME;
 
     private DbHelper dbHelper;
     private SQLiteDatabase db;
@@ -36,7 +41,7 @@ public class AccountContentProvider extends ContentProvider {
     public String getType(Uri uri) {
         switch (uriMatcher.match(uri)) {
             case TYPE_ACCOUNT_LIST:
-                return IProivderMetaData.AccountMetaData.LIST;
+                return IProivderMetaData.AccountColumns.LIST;
             default:
                 throw new IllegalArgumentException("This is a unKnow Uri"
                         + uri.toString());
@@ -88,5 +93,48 @@ public class AccountContentProvider extends ContentProvider {
                       String[] selectionArgs) {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    public static void insertAccount(Context context, AccountInfo accountInfo) {
+        if (context == null || accountInfo == null) {
+            return;
+        }
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_NAME, accountInfo.name);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_SEX, accountInfo.sex);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_TEL, accountInfo.tel);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_ADDRESS, accountInfo.address);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_COMPANY, accountInfo.company);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_ASSETS_TYPE, accountInfo.assetsType);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_CONSUMER_GRADE, accountInfo.consumerGrade);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_CARD_ID, accountInfo.cardId);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_FLAG, accountInfo.flag);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_ATTRIBUTION, accountInfo.attribution);
+        contentValues.put(IProivderMetaData.AccountColumns.COLUMNS_INTEGRAL, accountInfo.integral);
+        context.getContentResolver().insert(IProivderMetaData.AccountColumns.URI_ACCOUNT, contentValues);
+    }
+
+    public static List<AccountInfo> queryAll(Context context) {
+        List<AccountInfo> list = null;
+        if (context != null) {
+            list = new ArrayList<>();
+            Cursor query = context.getContentResolver().query(IProivderMetaData.AccountColumns.URI_ACCOUNT, null, null, null, null);
+            while (query != null && query.moveToNext()) {
+                AccountInfo accountInfo = new AccountInfo();
+                accountInfo.name = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_NAME));
+                accountInfo.sex = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_SEX));
+                accountInfo.tel = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_TEL));
+                accountInfo.address = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_ADDRESS));
+                accountInfo.company = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_COMPANY));
+                accountInfo.assetsType = query.getInt(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_ASSETS_TYPE));
+                accountInfo.consumerGrade = query.getInt(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_CONSUMER_GRADE));
+                accountInfo.cardId = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_CARD_ID));
+                accountInfo.flag = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_FLAG));
+                accountInfo.attribution = query.getString(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_ATTRIBUTION));
+                accountInfo.integral = query.getInt(query.getColumnIndex(IProivderMetaData.AccountColumns.COLUMNS_INTEGRAL));
+                list.add(accountInfo);
+            }
+        }
+        return list;
     }
 }
